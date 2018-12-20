4935cf0aad09a21c86aef676db6d6da36635c951
KillerDucks <nimesh.patel@bindserver.com>
2018-12-20 14:06:27 +0000

    Added ability to pass port number via CLI arguments

--- WebServer/WebServer.cpp
+++ WebServer/WebServer.cpp
@@ -5,9 +5,29 @@
 #include "Server.h"
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc < 1)
+	{
+		if (strlen((char*)argv[1]) == 4)
+		{
+			if (isdigit(argv[1][0]))
+			{
+				Server srv = Server((char*)argv[1]);
+				srv.ServerListen();
+
+				std::cout << "Exiting!\n";
+
+				return 1;
+			}
+		}
+	}
+		
+		
 	Server srv = Server((char*)"8080");
 	srv.ServerListen();
+
     std::cout << "Exiting!\n"; 
+
+	return 1;
 }
